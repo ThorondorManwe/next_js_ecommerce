18334192dfc7a5d7540fe79fc57eea85d86b5f21
Carlos Rangel <charlesleimaginant@gmail.com>
2025-06-29 20:16:36 -0600

    App Creation | Layout | Responsive Sheet menu

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,11 @@
-const config = {
+/*const config = {
   plugins: ["@tailwindcss/postcss"],
+};*/
+/** @type {import('tailwindcss').Config} */
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
 
-export default config;
+//export default config;
