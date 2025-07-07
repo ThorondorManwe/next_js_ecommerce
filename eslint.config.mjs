7a5aea769fc2e996ed15a500313719a19c7841a3
Carlos Rangel <charlesleimaginant@gmail.com>
2025-07-06 21:21:42 -0600

    Initial Deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignorePatterns: ["lib/generated/prisma/"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
